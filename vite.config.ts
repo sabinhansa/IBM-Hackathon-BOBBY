746d7880f84c916d3c8d15688a5c7267971a1254
Hânsă Sabin Ștefan <sabih.sh18@gmail.com>
2026-05-15 23:26:24 +0300

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BOBBY-LOVES-REPOS/',
+  base: '/IBM-Hackathon-BOBBY/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
